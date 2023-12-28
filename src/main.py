09ffc519450e0a73e9ed0782f845201c8d01f997
Flynn Alexander <flynn.alexander@halter.co.nz>
2023-12-28 17:26:32 +1300

    initial cleanup and project organisation

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("models/YOLO/yolov8n-pose.pt")  # load a pretrained model (recommended for training)
+
+results = model.predict(source="1", show=True)
