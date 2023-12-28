6ef7723772d5720bc857d3264836ef91e817cdd6
Flynn Alexander <flynn.alexander@halter.co.nz>
2023-12-28 17:45:10 +1300

    add timm to pipfile

--- src/main.py
+++ src/main.py
@@ -2,5 +2,4 @@ from ultralytics import YOLO
 
 # Load a model
 model = YOLO("models/YOLO/yolov8n-pose.pt")  # load a pretrained model (recommended for training)
-
-results = model.predict(source="1", show=True)
+results = model.predict(source="1", show=True)  # predict on the webcam stream
